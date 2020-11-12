7cb1561e3f00b13dcfbcac9883e889a4df24f1e0
Musa Usman <>
2020-11-12 13:02:24 +0500

    Part 4 - [0.0.2] - Thursday, 12th November 2020

--- lib/src/helpers.dart
+++ lib/src/helpers.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+/// Created by Musa Usman on 07.11.2020
+/// Copyright © 2020 Musa Usman. All rights reserved.
+///
+/// Email: hello@musausman.com
+/// Website: musausman.com
+/// WhatsApp: +92 324 9066001
+
 const double _defaultDesktopBreakpoint = 950;
 const double _defaultTabletBreakpoint = 600;
 const double _defaultWatchBreakpoint = 300;
